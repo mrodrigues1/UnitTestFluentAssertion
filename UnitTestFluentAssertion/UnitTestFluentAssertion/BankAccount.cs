a123920b8b49572d7b8bc76abeebc0d0c3f4a194
Matheus Rodrigues <matheus.moreira.rodrigues01@gmail.com>
2018-04-12 22:30:20 -0300

    First Commit

---
+++ UnitTestFluentAssertion/UnitTestFluentAssertion/BankAccount.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace UnitTestFluentAssertion
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        private bool frozen = false;
+
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (frozen)
+                throw new Exception("Account frozen");
+
+            if (amount > balance)
+                throw new ArgumentOutOfRangeException("Not enough balance.");
+
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException("Amount should be bigger than zero.");
+
+            balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (frozen)
+                throw new Exception("Account frozen");
+
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException("Amount should be bigger than zero.");
+
+            balance += amount;
+        }
+
+        public void FreezeAccount()
+        {
+            frozen = true;
+        }
+
+        public void UnfreezeAccount()
+        {
+            frozen = false;
+        }
+    }
+}
---
+++ UnitTestFluentAssertion/UnitTestFluentAssertion/BankAccountTests.cs
@@ -0,0 +1,97 @@
+using System;
+using Xunit;
+using FluentAssertions;
+
+namespace UnitTestFluentAssertion
+{
+    public class BankAccountTests
+    {
+        [Fact]
+        public void Credit_PositiveAmountAsParameter_BalanceEquals200()
+        {
+            //Arrange
+            var balance = 100;
+            var amount = 100;
+
+            var sut = new BankAccount(balance);
+
+            //Act
+            sut.Credit(amount);
+
+            //Assert
+            sut.Balance.Should().Be(200);
+        }
+
+        [Fact]
+        public void Credit_PositiveAmountAsParameter_BalanceTypeIsDouble()
+        {
+            //Arrange
+            var balance = 100;
+            var amount = 100;
+
+            var sut = new BankAccount(balance);
+
+            //Act
+            sut.Credit(amount);
+
+            //Assert
+            sut.Balance.Should().BeOfType(typeof(double));
+        }
+
+        [Fact]
+        public void Credit_NegativeAmountAsParameter_ThrowsArgumentOutOfRangeException()
+        {
+            //Arrange
+            var balance = 100;
+            var amount = -1;
+
+            var sut = new BankAccount(balance);
+
+            //Act
+            Action action = () => sut.Credit(amount);
+
+            //Assert
+            action
+                .Should()
+                .Throw<ArgumentOutOfRangeException>();
+        }        
+
+        [Fact]
+        public void Debit_AccountIsFrozen_ThrowsExceptionWithMessage()
+        {
+            //Arrange
+            var balance = 100;
+            var amount = 1000;
+
+            var sut = new BankAccount(balance);
+            
+            //Act
+            sut.FreezeAccount();
+            Action action = () => sut.Debit(amount);
+
+            //Assert
+            action
+                .Should()
+                .Throw<Exception>()
+                .WithMessage("Account frozen");
+        }
+
+        [Fact]
+        public void Debit_PositiveAmountAsParameter_NotThrowException()
+        {
+            //Arrange
+            var balance = 100;
+            var amount = 50;
+
+            var sut = new BankAccount(balance);
+            
+            //Act            
+            Action action = () => sut.Debit(amount);
+
+            //Assert
+            action
+                .Should()
+                .NotThrow<Exception>();
+        }
+    }
+}
